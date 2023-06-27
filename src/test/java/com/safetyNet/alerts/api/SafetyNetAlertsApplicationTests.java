f1df5caa71d28a2caa72bb6cedd4fb4db579a249
Florian Fortier <78166457+FlorianFortier@users.noreply.github.com>
2023-06-27 23:40:05 +0200

    init project

---
+++ src/test/java/com/safetyNet/alerts/api/SafetyNetAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safetyNet.alerts.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyNetAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
